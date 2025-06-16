525d3ff31de8737cd309032f50614ab35019c824
Prodelaya <proyectos.delaya@gmail.com>
2025-06-16 10:59:34 +0000

    Initial commit: En Escena Madrid ASP.NET Core MVC setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EnEscenaMadrid.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
